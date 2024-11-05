7cbbee421d5739936f29d89e4eef79d1e9d9b9fa
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-11-05 17:46:03 +0530

    refactor: move confirmation dir out of src

--- src/confirmation.ts
+++ src/confirmation.ts
@@ -6,7 +6,6 @@ const urlObject = new URL(url)
 const urlSearchParamsObject = new URLSearchParams(urlObject.search)
 
 const email = urlSearchParamsObject.get("email")
-console.log(email)
 
 if (emailPlaceholder) {
   emailPlaceholder.textContent = email
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-// vite.config.js
-import { resolve } from "path"
 import { defineConfig } from "vite"
+
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        confirmation: resolve(__dirname, "src/confirmation/index.html"),
+        main: path.resolve(__dirname, "index.html"),
+        confirmation: path.resolve(__dirname, "confirmation/index.html"),
       },
     },
   },
