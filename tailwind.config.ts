dc70a3eda1e186939459eceac708094f4f9a4937
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-11-05 17:55:38 +0530

    fix: confirmation page resulting in 404

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss"
 
 export default {
-  content: ["./*.html", "./src/**/*"],
+  content: ["./*.html", "./confirmation/*", "./src/**/*"],
   theme: {
     screens: {
       md: "58rem",
