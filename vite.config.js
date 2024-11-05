7f8bb1a9b982b9fcba35d0a9bb690dde7081d02a
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-11-05 16:53:03 +0530

    refactor: remove dirname creation

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
+// vite.config.js
+import { resolve } from "path"
 import { defineConfig } from "vite"
-
-import path from "path"
-import { fileURLToPath } from "url"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "index.html"),
-        confirmation: path.resolve(__dirname, "confirmation/index.html"),
+        main: resolve(__dirname, "index.html"),
+        confirmation: resolve(__dirname, "confirmation/index.html"),
       },
     },
   },
